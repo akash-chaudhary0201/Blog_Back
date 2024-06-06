49a3e0bfc65111b467cfff20e95bd1faa30b1c3f
Akash Chaudhary <akashchaudhary0201@gmail.com>
2024-06-06 21:44:26 +0530

    Solved the createPost error

--- controllers/postController.js
+++ controllers/postController.js
@@ -1,21 +1,13 @@
-const { Post } = require("../models/createPostModel");
+const Post = require("../models/createPostModel");
 
 exports.createPost = async (req, res) => {
   try {
     const { title, body } = req.body;
-    const post = await Post({
-      title,
-      body,
-    });
+    const post = new Post({ title, body });
     const savedPost = await post.save();
-
-    res.json({
-      post: savedPost,
-    });
+    res.json({ post: savedPost });
   } catch (error) {
     console.log(error);
-    return res.status(500).json({
-      error: "Error while creating a post",
-    });
+    return res.status(500).json({ error: "Error while creating a post" });
   }
 };
--- models/createPostModel.js
+++ models/createPostModel.js
@@ -1,26 +1,11 @@
 const mongoose = require("mongoose");
 
-const postSchema = new mongoose.Schema({
-  title: {
-    type: String,
-    required: true,
-  },
-  body: {
-    type: String,
-    required: true,
-  },
-  likes: [
-    {
-      type: mongoose.Schema.Types.ObjectId,
-      ref: "Like",
-    },
-  ],
-  comments: [
-    {
-      type: mongoose.Schema.Types.ObjectId,
-      ref: "Comment",
-    },
-  ],
+const PostSchema = new mongoose.Schema({
+  title: { type: String, required: true },
+  body: { type: String, required: true },
+  // other fields if any
 });
 
-module.exports = mongoose.model("Post", postSchema);
+const Post = mongoose.model("Post", PostSchema);
+
+module.exports = Post;
